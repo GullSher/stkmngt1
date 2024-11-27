1d3c34d832e364e9e8ddae0abafcff307902a1ce
GullSher <GullSher73@gamil.com>
2024-11-27 17:39:13 +0500

    adding a

---
+++ src/items.js
@@ -0,0 +1,10 @@
+// rfce after installing ES7 React/Redux/GraphQL/React-Native snippets extension
+import React from 'react'
+
+function items() {
+  return (
+    <div>items</div>
+  )
+}
+
+export default items
